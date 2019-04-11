aa54b76d8917222763274e71dbe256cf6dac94ac
Abdelaty Mohammedmagdi Abdelaty <Abdelaty.mohammedmagdi@gmail.com>
2019-04-11 08:31:31 +0200

    f

--- app/src/main/java/com/example/task1_abdelaty68472/InsertEmployee/InsertEmployee.java
+++ app/src/main/java/com/example/task1_abdelaty68472/InsertEmployee/InsertEmployee.java
@@ -2,6 +2,9 @@ package com.example.task1_abdelaty68472.InsertEmployee;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,35 +24,48 @@ import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class InsertEmployee extends AppCompatActivity {
+public class InsertEmployee extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @BindView(R.id.department_spinner)
     Spinner spinner;
+
     String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
-    @BindView(R.id.user_input)
-    EditText inputName;
-    @BindView(R.id.pass_input)
+    @BindView(R.id.employee_name_input)
+    EditText inputUser;
+
+    @BindView(R.id.employee_password_input)
     EditText inputPass;
+
     @BindView(R.id.employee_confirm_input)
     EditText inputPassConfirm;
+
     @BindView(R.id.employee_id_input)
     EditText inputId;
+
     @BindView(R.id.male_check)
     RadioButton radioMale;
+
     @BindView(R.id.female_check)
     RadioButton radioFemale;
+
     @BindView(R.id.insert_button)
     Button insertButton;
+
     ArrayList<String> departments = new ArrayList<>();
-    ArrayAdapter<String> adapter;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_employee);
+        ButterKnife.bind(this);
 //        Log.v("name", Environment.getExternalStorageDirectory().getAbsolutePath());
         readFromFile(path + "dep.txt");
-        adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_layout, departments);
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.row_layout, departments);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Log.v("hello", path);
+
 //        spinner.setOnItemSelectedListener(this);
 
         spinner.setAdapter(adapter);
@@ -65,7 +81,7 @@ public class InsertEmployee extends AppCompatActivity {
                 return;
             }
 
-            String studentText = inputId.getText().toString() + "$" + inputPass.getText().toString() + "$" + inputPassConfirm.getText().toString() + "$" + inputName.getText().toString() +
+            String studentText = inputId.getText().toString() + "$" + inputPass.getText().toString() + "$" + inputPassConfirm.getText().toString() + "$" + inputUser.getText().toString() +
                     "$" + spinner.getSelectedItem().toString() + "$" + gender;
             appendFile(path + "students.txt", studentText);
             Toast.makeText(getApplicationContext(), "Student Inserted", Toast.LENGTH_LONG).show();
@@ -102,4 +118,13 @@ public class InsertEmployee extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
